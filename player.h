b0d34976bf38a3f5e67c8a10973970d7ecdca8c5
trishamenon <79341522+trishamenon@users.noreply.github.com>
2021-02-19 18:48:54 -0500

    Create player.h

---
+++ player.h
@@ -0,0 +1,17 @@
+#include <string>
+
+class Player {
+
+	public:
+
+		Player ();
+		std::string name;
+		int id;
+		int runsScored;
+		int ballsPlayed;
+		int ballsBowled;
+		int runsGiven;
+		int wicketsTaken;
+
+
+};

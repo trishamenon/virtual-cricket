ebaa5684fb43a4e214a964b118d065fbb1a56eb5
trishamenon <79341522+trishamenon@users.noreply.github.com>
2021-02-19 18:50:13 -0500

    Create team.h

---
+++ team.h
@@ -0,0 +1,16 @@
+#include <vector>
+#include "player.h"  // <string>
+
+
+class Team {
+
+	public:
+		Team();
+		std::string name;
+		int totalRunsScored;
+		int wicketsLost;
+		int totalBallsBowled;
+		std::vector<Player> players;
+
+
+};
